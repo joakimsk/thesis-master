a365322fa79a65242e02c63f62a3336a33303b42
joakimsk <skjefstad.joakim@gmail.com>
2015-10-15 19:13:27 +0200

    First OpenCL Test

--- vs_project/vs_project/main.cpp
+++ vs_project/vs_project/main.cpp
@@ -1,7 +1,17 @@
 #include<stdio.h>
+#include<CL/cl.h>
+#include<iostream>
 
-int main(void) {
+int main(void)
+{
+	cl_int err;
+	cl_uint numPlatforms;
 
-	printf("Hello worls");
+	err = clGetPlatformIDs(0, NULL, &numPlatforms);
+	if (CL_SUCCESS == err)
+		printf("\nDetected OpenCL platforms: %d", numPlatforms);
+	else
+		printf("\nError calling clGetPlatformIDs. Error code: %d", err);
+	system("pause");
 	return 0;
 }
